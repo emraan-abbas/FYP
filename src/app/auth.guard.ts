90ccee78eef49a9420116547575c35078a2f5ce7
gshahg <imranabbas67@gmail.com>
2018-08-09 09:15:40 +0500

    Working, Error Removal

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate,Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
-import { userInfo } from 'os';
+
 
 @Injectable({
   providedIn: 'root'
